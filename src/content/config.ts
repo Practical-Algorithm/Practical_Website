239b88b3054a7b94b077b6ce28288448e013b4cb
Patiphon Loetsuthakun <36232545+Phon1209@users.noreply.github.com>
2024-06-21 22:51:48 +0700

    Add MDX support for Quiz

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,31 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const quiz = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    quizImage: z.string(),
+    answers: z.array(
+      z.object({
+        detail: z.string(),
+        image: z.string().optional(),
+      })
+    ),
+    correctAnswerIndex: z.number(),
+  }),
+});
+
+export const collections = { blog, quiz };
