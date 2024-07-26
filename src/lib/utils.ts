9063c89c2c5397ce0e1b45738be73cf98000469b
Patiphon Loetsuthakun <36232545+Phon1209@users.noreply.github.com>
2024-07-26 15:16:49 +0700

    Sanitize Layout

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type { CollectionEntry } from "astro:content"
+
+type BlogPostType = CollectionEntry<"blog">
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export function formatBlogPost(
+  blogs: BlogPostType[],
+  sortCriteria: (a: BlogPostType, b: BlogPostType) => number 
+  = (a, b) =>
+    new Date(b.frontmatter.date) - new Date(a.frontmatter.date),
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  limit = undefined
+) {
+  
 }
